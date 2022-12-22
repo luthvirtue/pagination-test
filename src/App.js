14db3d5d4560bc069fbbb1a071eeda5ab4b91def
luthvirtue <rifatila_muha@yahoo.com>
2022-12-22 18:29:46 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(2);
+  const [postPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://api.instantwebtools.net/v1/airlines')
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5'>
+      <h1 className="text-primary mb-3">My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination postsPerPage={postPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
 }
